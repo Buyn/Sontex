a9c09daf414d4b0fe74e34a29e5150a219a4a42d
buyn <buyn.tex@gmail.com>
2022-10-01 00:18:35 +0300

    add fest wersion of read

--- Src/sandbox.py
+++ Src/sandbox.py
@@ -1,1 +1,9 @@
- 
+import pandas as pd
+
+filename = "Data_files/metod01.xlsx"
+
+df = pd.read_excel(filename, sheet_name = "показники", engine='openpyxl')
+#view the first five rows: 
+print (df.head())
+
+
