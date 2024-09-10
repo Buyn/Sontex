ba3d5237348cf8ce8faf54aeeef2f34bbde08dc0
buyn <buyn.tex@gmail.com>
2024-09-10 23:56:27 +0300

    add build folder

---
+++ build/counter_values.py
@@ -0,0 +1,83 @@
+# ----------------------------------------------
+# * imports : 
+# ----------------------------------------------
+import pandas as pd
+from global_values import *
+
+
+# ----------------------------------------------
+# * class Counter_values :
+# ** ------------------------------------------:
+class Counter_values:
+
+
+# ** def __init__ : 
+    def __init__(self, df, line): 
+        self._df = df
+        self._line = line
+        self.adress = self.get_adress()
+        if not self.is_valid(): return 
+        self.value1 = self.get_value1()
+        self.value2 = self.get_value2()
+        self.k_priv = self.get_k_priv()
+
+        
+# ** def get_adress : 
+    def get_adress(self): 
+        return self._df.iloc[self._line, gl_counters_column]
+
+
+# ** def get_value1 : 
+    def get_value1(self): 
+        return self.get_value(gl_counters_value1_column, "gl_counters_value1_column")
+
+
+# ** def set_value1 : 
+    def set_value1(self, value): 
+        return self.set_value(gl_counters_value1_column, "gl_counters_value1_column", value)
+
+
+# ** def get_value2 : 
+    def get_value2(self): 
+        return self.get_value(gl_counters_value2_column, "gl_counters_value2_column")
+
+
+# ** def get_k_priv : 
+    def get_k_priv(self): 
+        return self.get_value(gl_counters_k_priv_column, "gl_counters_k_priv_column")
+
+
+# ** def gen_delta : 
+    def gen_delta(self): 
+        # споживання за період
+        return  self.value1 - self.value2
+
+
+# ** def gen_delta_k : 
+    def gen_delta_k(self): 
+        # споживання за період приведене
+        return  self.gen_delta() * self.k_priv
+
+
+# ** def get_value : 
+    def get_value(self, row, name): 
+        r = self._df.iloc[self._line, row]
+        if not isinstance(r, float) and not isinstance(r, int):
+            raise NameError('not int or float on line = ' + str(self._line + gl_exl_shift_rows) + ', for column ' + name)
+        if pd.isna(r):
+            raise NameError('no value on line = ' + str(self._line + gl_exl_shift_rows) + ', for column ' + name)
+        return r
+
+
+# ** def set_value : 
+    def set_value(self, row, name, value): 
+        r = self._df.iloc[self._line, row,] = value
+        return r
+
+
+# ** def is_valid : 
+    def is_valid(self): 
+        return  isinstance(self.adress, int)
+
+
+# * -------------------------------------------:
---
+++ build/rules.py
@@ -0,0 +1,108 @@
+# ----------------------------------------------
+# * imports:
+# ----------------------------------------------
+import pandas as pd
+from global_values import *
+
+
+# ----------------------------------------------
+# * Postprocessing functions:
+# ----------------------------------------------
+# **  def postproc_test:
+def postproc_test(df, arg):
+    if arg[0]!="test":
+        print(arg)
+    return df, arg
+
+
+# **  def postproc_divider:
+def postproc_divider(df:pd.DataFrame, arg):
+    # print("arg = ", arg)
+    # print("arg = ", arg[2])
+    strarg = arg[2].split("\"")
+    # print("strarg = ", strarg)
+    newrow = [i for i in strarg if i and i != " "]
+    # print("newrow = ", newrow)
+    # print(df.shape)
+    if df.shape[1] > len(newrow):
+        for col in range(df.shape[1] - len(newrow)):
+            newrow.append("")
+    # print(arg[0])
+    # print(df[df[0]==arg[0]])
+    target_index = int((df[df[0]==arg[0]].index.values)[0])
+    # tmp_int = df[df[0]==arg[0]].index.values
+    # print("tmp_int =", tmp_int)
+    # print("(int(tmp_int)) = ", int(tmp_int))
+    # target_index = int(tmp_int)
+    # print("index.values = ", target_index)
+    target_value = df.iloc[target_index, arg[1]-1]
+    # print("target_value = ", target_value)
+    value_k = arg[3]
+    # print("value_k = ", value_k)
+    new_value = float(target_value) * float(value_k)
+    df.iloc[target_index, arg[1]-1] = target_value - new_value
+    newrow[arg[1]-1] = new_value
+    # print(len(newrow))
+    # print(newrow)
+    # print(len(newrow))
+    # print(df)
+    #  Inserting the new row
+    # Inserting a Row at a Specific Index
+    df.loc[target_index + 0.5] = newrow
+    # print(target_index + 0.5)
+    #  Reset the index
+    df = df.sort_index().reset_index(drop=True)
+    # print(df)
+    return df, newrow
+
+
+# ** ------------------------------------------:
+# * vars:
+# ----------------------------------------------
+rules_dic = {
+    "test" : postproc_test,
+    "post_divider" : postproc_divider,
+}
+
+
+# ----------------------------------------------
+# * functions:
+# ----------------------------------------------
+# **  get_all_rules:
+def get_all_rules(df):
+    r = []
+    for i in range(0, df.shape[0]):
+        # print("i = ", i)
+        value_i = df.iloc[i, 0]
+        if df.iloc[i, 0] == "rule":
+            # print("rule found on index = ", i)
+            # print("value of i = ", df.iloc[i, 0])
+            ruls_name = df.iloc[i, 1]
+            ruls_params = df.iloc[i, 2]
+            ruls_params_list =[df.iloc[i, p] for p in range(3, 3 + ruls_params)]
+            r.append((i, ruls_name, ruls_params_list))
+    return r
+
+
+# **  use_rule:
+def use_rule(df, index, rule_name, rule_params, test = False):
+    try:
+        return rules_dic[rule_name](df, rule_params)
+    except Exception:
+        if not test:
+            print("no such rule in dictionary from row=",index+1, " ", rule_name)
+            # wm.print_to_log("немає такого правила у словнику з row="+ index+1+ " "+ rule_name)
+        return df, None
+
+# **  postprocessing_df_with_rules_df:
+def postprocessing_df_with_rules_df(df, df_rules):
+    rules_list = get_all_rules(df_rules)
+    if not rules_list: return df
+    for rule in rules_list: 
+        df, args = use_rule(df = df,
+                            index = rule[0],
+                            rule_name = rule[1],
+                            rule_params = rule[2],)
+    return df
+# ** ------------------------------------------:
+# * -------------------------------------------:
